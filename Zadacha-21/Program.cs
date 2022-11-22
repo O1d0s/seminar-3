c973f8af18f155fb6fb417722aeb86b875453749
Aidos <ajdos0735@gmaul.com>
2022-11-22 21:46:49 +0600

    решил задачу-21

---
+++ Zadacha-21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+Console.WriteLine("Напишите значение x,y,z для точки A: ");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Напишите значение x,y,z для точки B: ");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+
+Console.WriteLine();
+Console.WriteLine(AB);
