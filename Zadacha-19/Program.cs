0ecd5fb41d84bee5ed0c8eaaafcee63c8ac8d699
Aidos <ajdos0735@gmaul.com>
2022-11-22 21:27:39 +0600

    решил задачу-19

---
+++ Zadacha-19/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+string number1 = number.ToString();
+
+if (number1.Length != 5)
+{
+    Console.WriteLine("Такое значение не правильное!");
+}
+else
+{
+    if (number1[0] == number1[4] && number1[1] == number1[3]){
+        Console.WriteLine("Да");
+    }
+    else{
+        Console.WriteLine("Нет");
+    }
+}
